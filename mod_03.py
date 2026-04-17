c6e199f4b55847754436df726625456140ce984d
Tristan Louie Pineda <tlcpineda.projects@gmail.com>
2026-04-18 01:53:08 +0800

    Initial mod_03.py.

---
+++ mod_03.py
@@ -0,0 +1,225 @@
+import os
+
+from PIL import Image
+
+from lib import (
+    continue_sequence,
+    display_message,
+    display_path_desc,
+    ensure_path_exists,
+    identify_path,
+    welcome_sequence,
+)
+
+# Module variables
+mod_name = "Compile Revision Files"
+mod_ver = "1"
+date = "13 Apr 2026"
+email = "tlcpineda.projects@gmail.com"
+compile_dir_base = "COMPILED-JPG TO PDF"
+jpeg_res = 72  # dpi
+
+
+def compile_revision():
+    print(">>> Select source PSD folder ...")
+
+    path = str(identify_path("folder"))
+
+    if not path:
+        print("\n<=> No folder selected.")
+        return
+
+    input_path = os.path.normpath(path)  # Normalise path.
+
+    display_message("INFO", "Processing files in folder ...")
+
+    parent, base = display_path_desc(input_path, "folder")
+    psd_dir, jpeg_dir, pdf_name = gen_revision_pathnames(parent, base)
+    psd_files = filter_files(input_path)
+
+    if not psd_files:
+        print("\n<=> No PSD file fit to be compiled.")
+        return False
+
+    psd_files.sort()
+    for_pdf_pages = []
+
+    try:
+        # Ensure that destination folders exists.
+        ensure_path_exists(psd_dir, "folder")
+        ensure_path_exists(jpeg_dir, "folder")
+
+        for file_ind, filename in enumerate(psd_files):
+            source_file = os.path.join(input_path, filename)
+
+            display_message(
+                "INFO", f"Processing file {file_ind} of {len(psd_files)}..."
+            )
+            display_path_desc(source_file, "file")
+
+            # Create matchng JPEG file.
+            with Image.open(source_file) as img:
+                base_filename = os.path.splitext(filename)[0]
+                rgb_img = img.convert("RGB")
+
+                # Store copy of image to memory, for PDF compilation.
+                for_pdf_pages.append(rgb_img.copy())
+
+                save_jpeg(rgb_img, jpeg_dir, base_filename)
+                copy_psd(source_file, psd_dir, base_filename)
+
+        if for_pdf_pages:
+            compile_pdf_file(for_pdf_pages, pdf_name)
+
+        return True
+
+    except Exception as e:
+        display_message("ERROR", "Failed to compile revision folder.", f"{e}")
+
+        return False
+
+
+def save_jpeg(rgb: Image.Image, dest_path: str, jpeg_name: str) -> None:
+    jpeg_path = compile_dest_path(dest_path, jpeg_name, "jpg", "file")
+
+    try:
+        # Save JPEG to local directory.
+        rgb.save(jpeg_path, "JPEG", dpi=(jpeg_res, jpeg_res))
+
+        display_message("SUCCESS", "JPEG file saved to revision folder.")
+        display_path_desc(jpeg_path, "file")
+
+    except Exception as e:
+        display_message("ERROR", "Failed to save file.", f"{e}")
+
+
+def copy_psd(source_path: str, dest_path: str, psd_name: str) -> None:
+    # Creates a copy of the PSD file (Manual Binary Copy) to revision folder.
+    psd_path = compile_dest_path(dest_path, psd_name, "psd", "file")
+
+    try:
+        with open(source_path, "rb") as f_src:
+            with open(psd_path, "wb") as f_dst:
+                # Copying in 1MB chunks to be safe with large PSD files
+                while True:
+                    chunk = f_src.read(1024 * 1024)
+                    if not chunk:
+                        break
+                    f_dst.write(chunk)
+
+        display_message("SUCCESS", "PSD file copied to revision folder.")
+        display_path_desc(psd_path, "file")
+
+    except Exception as e:
+        display_message("ERROR", "Failed to copy PSD file.", f"{e}")
+
+
+def compile_pdf_file(imgs: list, pdf_path: str) -> None:
+    try:
+        imgs[0].save(
+            pdf_path, "PDF", resolution=jpeg_res, save_all=True, append_images=imgs[1:]
+        )
+        display_message("SUCCESS", "Revision PDF file created.")
+        display_path_desc(pdf_path, "file")
+
+    except Exception as e:
+        display_message("ERROR", "Failed to compile revision PDF file.", f"{e}")
+
+
+def compile_dest_path(
+    parent_dir: str, basename: str, extname: str, pathtype: str
+) -> str:
+    if pathtype == "file":
+        dest_path = os.path.join(parent_dir, f"{basename}.{extname}")
+    else:  # folder
+        dest_path = os.path.join(parent_dir, f"{basename}/{extname}")
+
+    return os.path.normpath(dest_path)
+
+
+def gen_revision_pathnames(parent_dir, basename) -> tuple[str, ...]:
+    psd_path = ""
+    jpeg_path = ""
+    pdf_path = ""
+    ch_num = parent_dir.split("-")[-1].strip()
+    current_num_rev_dir = count_rev_dirs(parent_dir) + 1
+
+    display_message(
+        "INFO",
+        f"Creating pathnames for {ch_num} (rev. {current_num_rev_dir}) ...",
+    )
+
+    try:
+        current_rev_dir = f"{compile_dir_base}{current_num_rev_dir}"
+
+        psd_path = compile_dest_path(parent_dir, current_rev_dir, "PSD", "folder")
+        jpeg_path = compile_dest_path(parent_dir, current_rev_dir, "JPEG", "folder")
+        pdf_path = compile_dest_path(
+            os.path.join(parent_dir, current_rev_dir), ch_num, "pdf", "file"
+        )
+
+        for path in [psd_path, jpeg_path, pdf_path]:
+            display_path_desc(path, "file" if path == pdf_path else "folder")
+
+        display_message("SUCCESS", "Path names created.  Pending actual creation.")
+
+    except Exception as e:
+        display_message("ERROR", "Failed to generated revision directory.", f"{e}")
+
+    return psd_path, jpeg_path, pdf_path
+
+
+def count_rev_dirs(directory: str) -> int:
+    rev_dirs = [d for d in os.listdir(directory) if d.startswith(compile_dir_base)]
+
+    return len(rev_dirs)
+
+
+def filter_files(source_path: str) -> list:
+    """
+    Filter files that follow the filename pattern, with the last two/three digits as the page markers.
+    :param folder: The parent folder of the PSD files
+    :return filtered_files: The list of filtered PSD files to be compiled
+    """
+    display_message("INFO", "Filtering files ...")
+    parent_folder, base_folder = display_path_desc(source_path, "folder")
+
+    source_files = os.listdir(source_path)
+
+    try:
+        filtered_files = []
+
+        for f in source_files:
+            filename, extname = os.path.splitext(f)
+
+            if (
+                filename.startswith(base_folder) and extname.lower() == ".psd"
+            ):  # Append file to return list if page_marker exists.
+                filtered_files.append(f)
+                print(f"\n<=>  {f} -> INCLUDE")
+            else:
+                print(f"\n<=>  {f} -> EXCLUDE")
+
+        display_message(
+            "SUCCESS",
+            f"Total of {len(filtered_files)} out of {len(source_files)} files found.",
+        )
+
+        return filtered_files
+
+    except Exception as e:
+        display_message("ERROR", f"Failed to filter files in {source_path}.", f"{e}")
+
+        return []  # Return empty list.
+
+
+if __name__ == "__main__":
+    welcome_sequence([mod_name, f"ver {mod_ver} {date}", email])
+
+    print(input("\n>>> Press enter to continue ..."))
+
+    confirm_exit = False
+
+    while not confirm_exit:
+        compile_revision()
+        confirm_exit = continue_sequence()
